289299d133fc2d5155ca850c9265d20ef74f02bd
Junyu Zhan <thezjy@gmail.com>
2021-08-06 10:43:05 +0800

    download cert using node instead of curl

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const helmet = require('helmet')
 const { execSync } = require('child_process')
 const fs = require('fs')
 const morgan = require('morgan')
+const https = require('https')
 
 const router = require('./router')
 
@@ -13,33 +14,48 @@ const COCKROACH_CERT_PATH = '/data/close-bison-ca.crt'
 const isProduction = process.env.FLY_APP_NAME != null
 
 if (isProduction) {
-  downloadCockroachCert()
+  downloadCockroachCert(start)
+} else {
+  start()
 }
 
-const { PORT } = process.env
+function start() {
+  const { PORT } = process.env
 
-const app = express()
+  const app = express()
 
-app.use(morgan(isProduction ? 'combined' : 'dev'))
-app.use(express.json())
-app.use(express.urlencoded())
+  app.use(morgan(isProduction ? 'combined' : 'dev'))
+  app.use(express.json())
+  app.use(express.urlencoded())
 
-// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Max-Age
-app.use(cors({ maxAge: 86400 }))
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Max-Age
+  app.use(cors({ maxAge: 86400 }))
 
-app.use(helmet())
-app.use(router)
+  app.use(helmet())
+  app.use(router)
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`)
-})
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+  })
+}
 
-function downloadCockroachCert() {
+function downloadCockroachCert(start) {
   if (!fs.existsSync(COCKROACH_CERT_PATH)) {
     console.log('try download cockroach cert')
-    execSync(
-      `curl --create-dirs -o ${COCKROACH_CERT_PATH} -O https://cockroachlabs.cloud/clusters/a0d63d14-0733-4ebe-9a7d-361d540a4db2/cert`,
+
+    https.get(
+      'https://cockroachlabs.cloud/clusters/a0d63d14-0733-4ebe-9a7d-361d540a4db2/cert',
+      (res) => {
+        const filePath = fs.createWriteStream(COCKROACH_CERT_PATH)
+        res.pipe(filePath)
+        filePath.on('finish', () => {
+          filePath.close()
+          console.log('download cockroach cert successful')
+          start()
+        })
+      },
     )
-    console.log('download cockroach cert successful')
+  } else {
+    start()
   }
 }
